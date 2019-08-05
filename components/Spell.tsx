a208e355f35a774383645359db3fb3a61deb5bbd
Lucas Shadler <lucas_shadler@intuit.com>
2019-08-04 23:14:59 -0700

    spell modal

--- components/Spell.tsx
+++ components/Spell.tsx
@@ -1,22 +1,80 @@
-import React from "react";
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from "react";
+import {StyleSheet, Text, View, Modal, TouchableHighlight, Alert} from 'react-native';
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function Spell({spell}) {
     const {name, subtype, slot, effect, enhanced, type, pool} = spell;
-    return (<View style={styles.container}>
-        <Text>{name}</Text>
-        <Text>{type}</Text>
-        <Text>{effect}</Text>
-    </View>)
+    const [isModalVisible, toggleModal] = useState(false);
+
+    return (
+        <TouchableHighlight  onPress={() => {toggleModal(!isModalVisible)}}>
+            <View style={styles.container}>
+                <View style={styles.type}>
+                    <Text>{type}</Text>
+                </View>
+                <View style={styles.details}>
+                    <Text>{name}</Text>
+                </View>
+
+                <Modal
+                    animationType="fade"
+                    transparent={false}
+                    visible={isModalVisible}
+                    onRequestClose={() => {
+                        toggleModal(!isModalVisible);
+                    }}>
+                    <View style={styles.modal}>
+                        <View>
+                            <TouchableHighlight
+                                onPress={() => {
+                                    toggleModal(!isModalVisible);
+                                }}>
+                                <Icon name='close' size={25} style={styles.close} />
+                            </TouchableHighlight>
+                            <Text>{name}{subtype ? ` - ${subtype}` : ""}</Text>
+                            <Text>{`Element: ${type}`}</Text>
+                            <Text>{`Arcana Slot: ${slot}`}</Text>
+                            <Text>{`Description: ${effect}`}</Text>
+                            <Text>{`Enhanced: ${enhanced}`}</Text>
+                            <Text>{`Pool: ${pool}`}</Text>
+
+
+                        </View>
+                    </View>
+                </Modal>
+
+            </View>
+        </TouchableHighlight>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
+        maxWidth: '100%',
+        height: 60,
+        flexDirection: 'row',
+        justifyContent: "flex-start",
         alignItems: "center",
         borderColor: 'gray',
+        backgroundColor: '#ffffff',
         borderWidth: 1,
         marginTop: 2
+    },
+    type: {
+        flexShrink: 0,
+        minWidth: "20%",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    details: {
+        flexGrow: 4
+    },
+    modal: {
+        padding: 10,
+        margin: 20
+    },
+    close: {
+        alignSelf: "flex-end"
     }
 });
--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {StyleSheet, Text, View, TextInput, FlatList} from 'react-native';
+import {StyleSheet, Text, KeyboardAvoidingView, TextInput, FlatList, KeyboardAvoidingViewComponent} from 'react-native';
 import Fuse from 'fuse.js';
 import SpellComponent from "../components/Spell";
 
@@ -48,32 +48,37 @@ export default function Search() {
     const [search, updateSearch] = useState("");
 
     return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
         <TextInput
             style={styles.input}
             onChangeText={updateSearch}
             value={search}
+            placeholder="Search Spells"
         />
         <FlatList
             style={styles.list}
             data={spellSearch.search(search)}
             renderItem={(d) => (<SpellComponent key={d.item.name} spell={d.item}/>)}
         />
-    </View>
+    </KeyboardAvoidingView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: "10%",
+        padding: "5%",
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        backgroundColor: "#4b4b4b"
     },
     input: {
         height: 40,
-        width: "80%",
+        marginTop: 20,
+        width: "100%",
         borderColor: 'gray',
+        backgroundColor: "#ffffff",
+        padding: 5,
         borderWidth: 1
     },
     list: {
